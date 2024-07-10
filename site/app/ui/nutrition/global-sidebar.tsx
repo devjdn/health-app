746438cc09a915b7ac46c61876ee6006fe66dd0c
Jayden Priestley <153106449+jhpriestley16@users.noreply.github.com>
2024-07-10 00:53:56 +0000

    made the nav menu toggleable on the sidebar to avoid overcrowding

--- site/app/ui/nutrition/global-sidebar.tsx
+++ site/app/ui/nutrition/global-sidebar.tsx
@@ -3,16 +3,28 @@
 import Link from "next/link";
 import clsx from 'clsx';
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+
+interface LinkType {
+  name: string;
+  href: string;
+}
+
+interface PageType {
+  pageName: string;
+  links: LinkType[];
+}
 
 export default function Sidebar() {
 
     const pathname = usePathname();
 
-    const pages = [
+    const pages: PageType[] = [
       {
         pageName: "Nutrition",
         links: [
-          { name: "Home", href: "/nutrition" },
+          // { name: "Home", href: "/nutrition" },
           { name: "Nutritional analysis", href: "/nutrition/analysis" },
           { name: "Recipe search", href: "/nutrition/recipes" },
           { name: "Meal planner", href: "/nutrition/meal-planner" },
@@ -22,7 +34,7 @@ export default function Sidebar() {
       {
         pageName: "Exercise",
         links: [
-          { name: "Home", href: "/exercise" },
+          // { name: "Home", href: "/exercise" },
           { name: "Fitness plans", href: "/exercise/link1" },
           { name: "Effect analysis", href: "/exercise/link2" },
           { name: "Fitness resources", href: "/exercise/link3" },
@@ -32,7 +44,7 @@ export default function Sidebar() {
         pageName: "Interact",
         links: [
           {name: "AI with Gemini", href: "/ai"},
-          {name: "Tracking and Progress", href: "tracking-and-progress"},
+          {name: "Tracking and Progress", href: "/tracking-and-progress"},
         ]
       },
       {
@@ -45,32 +57,53 @@ export default function Sidebar() {
         ],
       },
     ];
-      
 
-    return(
-        <div className="sidebar">
-            <div className="sidebar-intro">
-                <a href="/"><h3>Zen</h3></a>
-            </div>
-            <nav className="sidebar-links">
-              {pages.map((page, index) => (
-                <div className="link-group" key={index}>
-                  <p className="link-group-title">{page.pageName}</p>
-                  <ul className="link-group-list">
-                    {page.links.map((link, linkIndex) => (
-                      <Link href={link.href} key={linkIndex}>
-                        <li className="link-group-item" key={linkIndex}>
-                          {link.name}
-                        </li>
-                      </Link>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            </nav>
-            <div className="sidebar-quick-settings">
+    // Create a state for each nav group
+    const [openSideNavGroups, setOpenSideNavGroups] = useState<boolean[]>(
+      pages.map(() => false)
+    );
 
-            </div>
+    // Toggle specific nav group
+    const toggleNavGroup = (index: number) => {
+      setOpenSideNavGroups(openSideNavGroups.map((isOpen, i) => 
+        i === index ? !isOpen : isOpen
+      ));
+    };
+
+    return (
+      <div className="sidebar">
+        <div className="sidebar-intro">
+          <a href="/"><h3>Zen</h3></a>
         </div>
+        <nav className="sidebar-links">
+          {pages.map((page, index) => (
+            <div className="link-group" key={index}>
+              <button
+                className="nav-group-opener"
+                onClick={() => toggleNavGroup(index)}
+                aria-expanded={openSideNavGroups[index]}
+              >
+                <p className="link-group-title">{page.pageName}</p>
+                <ChevronDown size={20} strokeWidth="1" />
+              </button>
+              <ul
+                className="link-group-list"
+                aria-hidden={!openSideNavGroups[index]}
+              >
+                {page.links.map((link, linkIndex) => (
+                  <Link href={link.href} key={linkIndex}>
+                    <li className="link-group-item" key={linkIndex}>
+                      {link.name}
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </nav>
+        <div className="sidebar-quick-settings">
+
+        </div>
+      </div>
     );
 }
