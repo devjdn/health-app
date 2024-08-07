6e394b0d1c2a864ae827684bc956305597360806
Jayden Priestley <153106449+devjdn@users.noreply.github.com>
2024-08-07 22:47:37 +0000

    so stuck for ideas

--- site/components/dashboard/dashboard-nav-bar.tsx
+++ site/components/dashboard/dashboard-nav-bar.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { PanelLeftOpen, PanelLeftClose } from "lucide-react";
+import { PanelLeftOpen, PanelLeftClose, Wheat, Dumbbell, BrainCircuit, HandHeart } from "lucide-react";
+import Link from "next/link";
 
 export default function DashboardNavBar(){
     const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -10,15 +11,41 @@ export default function DashboardNavBar(){
         setIsOpen(!isOpen)
     }
 
+    const navLinks = [
+        { name: 'Nutrition', href: '/dashboard/nutrition', icon: <Wheat strokeWidth={1.5}/> },
+        { name: 'Exercise', href: '/dashboard/exercise', icon: <Dumbbell strokeWidth={1.5}/> },
+        { name: 'AI', href: '/dashboard/ai-with-gemini', icon: <BrainCircuit strokeWidth={1.5}/> },
+        { name: 'Wellbeing', href: '/dashboard/wellbeing', icon: <HandHeart strokeWidth={1.5}/> },
+    ]
+
     return(
         <div className={`dashboard-nav-bar ${isOpen ? 'open' : 'closed'}`} id="navbar" aria-expanded={isOpen}>
             <button className="nav-bar-opener" onClick={toggleNav}>
                 {isOpen ? (
-                    <PanelLeftOpen strokeWidth={1.5}/>
-                ) : (
+                    <>
                     <PanelLeftClose strokeWidth={1.5}/>
+                    <p>Close</p>
+                    </>
+                ) : (
+                    <>
+                    <PanelLeftOpen strokeWidth={1.5}/>
+                    </>
                 )}
             </button>
+            <nav className="dashboard-nav">
+                <ul className="nav-list">
+                    {navLinks.map((navLink, linkIndex) => (
+                        <li className="nav-list-item" key={linkIndex}>
+                            <Link href={navLink.href}>
+                                <div className="link-content">
+                                    {navLink.icon}
+                                    <p>{navLink.name}</p>
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </div>
     );
 }
